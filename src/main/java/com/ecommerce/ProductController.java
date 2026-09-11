dcb46228f79c5cd1133e66d8246b3d0180fd722f
bala <bala@gmail.com>
2026-09-11 18:12:29 +0530

    Initial e-commerce application

---
+++ src/main/java/com/ecommerce/ProductController.java
@@ -0,0 +1,13 @@
+package com.ecommerce;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @GetMapping("/products")
+    public String getProducts() {
+        return "E-Commerce Products";
+    }
+}
